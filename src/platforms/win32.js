a108d84e392e8b28f97ceb48e64b3b1e1d176ba9
Josh Grant <josh@synergitech.co.uk>
2017-08-31 13:02:53 +0100

    Fix wake() on win32

--- src/platforms/win32.js
+++ src/platforms/win32.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const ffi = require("ffi");
+const ref = require('ref');
+const struct = require('ref-struct');
 const os = require("os");
 
 const HWND_BROADCAST = 0xffff;
@@ -12,17 +14,42 @@ const POWER_ON = -0x0001;
 const win32 = {};
 
 win32.sleep = () => {
-	var user32 = ffi.Library("user32", {
-		SendMessageW: ["int", ["ulong", "uint", "long", "long"]]
-	});
-	user32.SendMessageW(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, POWER_OFF);
+    var user32 = ffi.Library("user32", {
+        SendMessageW: ["int", ["ulong", "uint", "long", "long"]]
+    });
+    user32.SendMessageW(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, POWER_OFF);
 }
 
 win32.wake = () => {
-	var user32 = ffi.Library("user32", {
-		SendMessageW: ["int", ["ulong", "uint", "long", "long"]]
-	});
-	user32.SendMessageW(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, POWER_ON);
+    var sendMessageW = ffi.Library("user32", {
+        SendMessageW: ["int", ["ulong", "uint", "long", "long"]]
+    });
+    var MouseInput = struct({
+        'type': 'int',
+        'dx': 'long',
+        'dy': 'long',
+        'mouseData': 'int',
+        'dwFlags': 'int',
+        'time': 'int',
+        'dwExtraInfo': 'int'
+    })
+    var MouseInputPtr = ref.refType(MouseInput);
+    var mouseInput = new MouseInput();
+    mouseInput.type = 0;
+    mouseInput.dx = 0;
+    mouseInput.dy = 0;
+    mouseInput.dwFlags = 0x0002;
+    mouseInput.mouseData = 0;
+    mouseInput.time = 0;
+    mouseInput.dwExtraInfo = 0;
+
+    var sendInput = ffi.Library('user32', {
+        'SendInput': ['int', ['uint', MouseInputPtr, 'int']]
+    });
+
+    sendInput.SendInput(1, mouseInput.ref(), (os.arch() == 'x64' ? 40 : 28));
+    // sendInput.SendInput(1, mouseInput.ref(), 40);
+    sendMessageW.SendMessageW(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, POWER_ON);
 }
 
 win32.supported = () => {
