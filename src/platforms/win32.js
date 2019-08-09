88c19f4835613d7dffddd315e458f18a49012723
Luke Bradshaw <lukebradshaw255@gmail.com>
2019-08-09 11:12:33 +0100

    Test new mouse move

--- src/platforms/win32.js
+++ src/platforms/win32.js
@@ -1,8 +1,6 @@
 "use strict";
 
 const ffi = require("ffi");
-const ref = require('ref');
-const struct = require('ref-struct');
 const os = require("os");
 
 const HWND_BROADCAST = 0xffff;
@@ -13,44 +11,17 @@ const POWER_ON = -0x0001;
 
 const win32 = {};
 
+const user32 = ffi.Library('user32', {
+    SendMessageW: ['int', ['ulong', 'uint', 'long', 'long']]
+});
 win32.sleep = () => {
-    var user32 = ffi.Library("user32", {
-        SendMessageW: ["int", ["ulong", "uint", "long", "long"]]
-    });
     user32.SendMessageW(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, POWER_OFF);
 }
 
 win32.wake = () => {
-    var sendMessageW = ffi.Library("user32", {
-        SendMessageW: ["int", ["ulong", "uint", "long", "long"]]
-    });
-    var MouseInput = struct({
-        'type': 'int',
-        'dx': 'long',
-        'dy': 'long',
-        'mouseData': 'int',
-        'dwFlags': 'int',
-        'time': 'int',
-        'dwExtraInfo': 'int'
-    })
-    var MouseInputPtr = ref.refType(MouseInput);
-    var mouseInput = new MouseInput();
-    mouseInput.type = 0;
-    mouseInput.dx = 0;
-    mouseInput.dy = 0;
-    mouseInput.dwFlags = 0x0002;
-    mouseInput.mouseData = 0;
-    mouseInput.time = 0;
-    mouseInput.dwExtraInfo = 0;
-
-    var sendInput = ffi.Library('user32', {
-        'SendInput': ['int', ['uint', MouseInputPtr, 'int']]
-    });
-
-    sendInput.SendInput(1, mouseInput.ref(), (os.arch() == 'x64' ? 40 : 28));
-    sendMessageW.SendMessageW(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, POWER_ON);
 }
-
+    user32.mouse_event(1, 40, 0, 0, 0);
+    user32.SendMessageW(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, POWER_ON);
 win32.supported = () => {
     return os.platform == 'win32';
 }
