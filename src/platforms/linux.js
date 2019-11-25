c388e1c5e27ee96942eba30875dd4c9219462339
Josh Grant <josh@synergitech.co.uk>
2019-11-25 22:24:38 +0000

    set default DISPLAY on linux

--- src/platforms/linux.js
+++ src/platforms/linux.js
@@ -2,7 +2,7 @@
 const exec = require('child_process').exec;
 const os = require('os');
 const linux = {};
-const DISPLAY = process.env.DISPLAY;
+const DISPLAY = process.env.DISPLAY || ':0';
 
 linux.sleep = () => {
     exec(`export DISPLAY=${DISPLAY}; xset dpms force suspend`);
